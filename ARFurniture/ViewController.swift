7cb8ad4b2638367a3ecddc9f406812ce934c6220
Akshay Relekar <arelekar08@gmail.com>
2019-11-30 15:00:22 -0500

    Testing Commit from ARBranch

--- ARFurniture/ViewController.swift
+++ ARFurniture/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("testing commit")
+        print("ARBranch Test Commit")
     }
 
 
